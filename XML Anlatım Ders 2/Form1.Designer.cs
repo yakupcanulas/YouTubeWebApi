4c9f5cfc25294e829765d5e3441830369b5fab3f
yakupcanulas <yakupcan_ulas@hotmail.com>
2017-11-29 22:31:41 +0300

    Proje Eklendi

---
+++ XML Anlatım Ders 2/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace XML_Anlatım_Ders_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Başlık = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Link = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(22, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Arama:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(357, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ara";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(109, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Başlık,
+            this.Link});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(25, 74);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(425, 165);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // Başlık
+            // 
+            this.Başlık.Text = "Başlık";
+            this.Başlık.Width = 169;
+            // 
+            // Link
+            // 
+            this.Link.Text = "link";
+            this.Link.Width = 252;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 261);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Başlık;
+        private System.Windows.Forms.ColumnHeader Link;
+    }
+}
+
---
+++ XML Anlatım Ders 2/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XML_Anlatım_Ders_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count>0)
+            {
+                string link = listView1.SelectedItems[0].SubItems[1].Text;
+
+
+                //listviewdeki linke tıkladığımızda linki açacak komut
+
+                System.Diagnostics.Process.Start(link);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            listView1.Items.Clear();
+
+            string url = string.Format("https://www.youtube.com/feeds/videos.xml?channel_id=UCGvj8kfUV5Q6lzECIrGY19g", textBox1.Text);
+
+            WebClient wc = new WebClient(); // webclient ile siteden indrilmesini sağlıyoruz aşağıdaki kodlar bu işe yarıyor.
+
+            string XmlData = wc.DownloadString(url);
+
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.LoadXml(XmlData);
+
+            //eğer xml in kodları karışık ise düzgün yazılmamış ise;
+
+            XmlNodeList entries = xDoc.DocumentElement.GetElementsByTagName("entry"); // burda çekeceğimiz bilginin hangi başlık altında olduğunu belirtiyoruz.
+
+            foreach (XmlNode entry in entries)
+            {
+
+                string title = entry.ChildNodes[3].InnerText;
+
+                if (!string.IsNullOrEmpty(title) && title.ToLower().Contains(textBox1.Text.ToLower()))
+                {
+
+                    ListViewItem lvi = new ListViewItem();
+
+                    lvi.Text = title;
+                    lvi.SubItems.Add(entry.ChildNodes[4].Attributes["href"].InnerText);
+
+                    listView1.Items.Add(lvi);
+
+
+
+
+                }
+
+
+            }
+        }
+    }
+}
